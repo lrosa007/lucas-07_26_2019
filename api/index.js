e33597d3aa7b64db3cdd3e51f15cec821821153b
lrosa007 <lucas@10factory.com>
2019-07-27 11:18:38 -0400

    feat: simple API to map out endpoints

--- api/index.js
+++ api/index.js
@@ -1,20 +1,50 @@
 const express = require("express");
+const { IncomingForm } = require("formidable");
 
 const app = express();
 const PORT = 3000;
 
-const tempData = [
+let tempData = [
   {
+    id: "1",
     name: "Upload 1"
   },
   {
+    id: "2",
     name: "Upload 2"
   },
   {
+    id: "3",
     name: "Upload 3"
   }
 ];
 
-app.get("/", (_req, res) => res.status(200).json({ data: tempData }));
+app.get("/documents", (req, res) => {
+  res.status(200).json({
+    data: Object.entries(req.query).reduce((documents, [name, value]) => {
+      return documents.filter(
+        document => !document[name] || document[name].includes(value)
+      );
+    }, tempData)
+  });
+});
+
+app.post("/documents", (req, res) => {
+  const form = new IncomingForm();
+
+  form.on("file", (field, file) => {
+    console.log(file);
+  });
+
+  form.on("end", () => {
+    res.status(200).json();
+  });
+});
+
+app.delete("/documents/:id", (req, res) => {
+  tempData = tempData.filter(document => document.id !== req.params.id);
+
+  res.status(200).json();
+});
 
 app.listen(PORT, () => console.log(`Upload API Running on port: ${PORT} 🚀`));
